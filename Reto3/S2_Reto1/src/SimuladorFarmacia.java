636d8fe2d56c67e8b97cd96e425bc19a4d3077c1
Elisa Tovar <perez.anaelisa6im01@gmail.com>
2025-04-29 17:57:56 -0600

    Add files via upload

---
+++ Reto3/S2_Reto1/src/SimuladorFarmacia.java
@@ -0,0 +1,22 @@
+public class SimuladorFarmacia {
+    String Nombre;
+    double PrecioU, Total, TotalAPagar, Des;
+    int Cantidad;
+    boolean Descuento;
+
+    public void Medicamento(){
+        Total = PrecioU * Cantidad;
+        Descuento = Total > 500;
+        //variable= (condition) ?expressionTrue:expressionFalse;
+        Des = Descuento ? Total*0.15: 0;
+        TotalAPagar = Total-Des;
+
+        System.out.println("\nMedicamento: " + Nombre);
+        System.out.println("Cantidad: " + Cantidad);
+        System.out.println("Precio unitario: $" + PrecioU);
+        System.out.println("Total sin descuento: $" + Total);
+        System.out.println("¿Aplica descuento?: " + Descuento);
+        System.out.println("Descuento: $" + Des);
+        System.out.println("Total a pagar: $" + TotalAPagar);
+    }
+}
